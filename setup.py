aa0c31f1d8804746bc67d10edba1c258d8d39220
Olha Maslova <olh.maslova@gmail.com>
2021-06-22 17:03:21 -0400

    initial commit. 2 tests fail

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions',
+      version='0.1',
+      desciption='Gaussian distributions',
+      package=['distributions'],
+      zip_safe=False)
+
